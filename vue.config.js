1e01c206a84a8be0ca0d7f0846bc99fbbcaef320
Lingyv77 <Lingyv77@163.com>
2021-01-23 22:11:11 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
     proxy: {
       '/api': {
         // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
-        target: 'https://search-x.jd.com/Search?callback=jQuery5473245&area=21&enc=utf-8&keyword=%E6%9C%8D%E8%A3%85%E5%A5%B3%E6%BD%AE%E6%B5%81&adType=7&page=1&ad_ids=291%3A33&xtest=new_search&_=1611398589572',
+        target: 'https://act.mogujie.com/ruqiu00001?acm=3.mce.2_10_1ji16.43542.0.ccy5br4OlfK0R.pos_1-m_454889-sd_119',
         // 允许跨域
         changeOrigin: true,
         ws: true,
