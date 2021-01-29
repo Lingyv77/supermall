b093c7f150a7047cb6690ac735efdd72134270b6
Lingyv77 <Lingyv77@163.com>
2021-01-29 11:48:52 +0800

    29

--- src/views/detail/childComps/DetailBaseInfo.vue
+++ src/views/detail/childComps/DetailBaseInfo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="Object.keys(goods).length" class="base-info">
+    <div v-if="Object.keys(goods).length !== 0" class="base-info">
         <div class="info-title">{{goods.title}}</div>
         <div class="info-price">
             <span class="new-price">{{goods.price}}</span>
