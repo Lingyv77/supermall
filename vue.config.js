4429e956bef4716f6444fe028a9e033551fdcaf3
Lingyv77 <Lingyv77@163.com>
2021-02-01 17:38:11 +0800

    稳

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   configureWebpack: {
     resolve: {
@@ -13,5 +15,8 @@ module.exports = {
       open: false,
       port: 8080, // 自定义修改8080端口
     }
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin('所有版权归Lingyv77所有')
+  ]
 }
